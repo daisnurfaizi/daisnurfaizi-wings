39878b9972732827582844d50334e4bd5767630e
daisnurfaizi <dsnurfaizi@gmail.com>
2023-04-02 22:43:57 +0700

    auto ci cd test

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -43,7 +43,7 @@
                         'text-gray-100': mode === 'dark',
                         'text-gray-900': mode === 'light'
                     }">
-                    Login </h3>
+                    Login Aplikasi</h3>
                 <form class="space-y-6" action="{{ route('cekLogin') }}" method="POST">
                     @csrf
                     <div>
